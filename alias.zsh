0abad0ce9f5d26713dc0d0ae6e0b4064ea8b9539
Eduard <ylane9@live.fr>
2022-10-21 05:48:21 +0200

    ygg resolv.conf

--- alias.zsh
+++ alias.zsh
@@ -2,3 +2,4 @@ alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
 alias vi="nvim"
 alias cat='bat --style=plain'
+alias ygg="sudo sh ~/Desktop/Other/script_resolv.sh"
