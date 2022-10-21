9517a54e7b73d4f81bbd0ab543e9940618061257
Eduard <ylane9@live.fr>
2022-10-21 04:55:00 +0200

    tab=4 s/o thePrimeagen, hlsearch..

--- additional_set.lua
+++ additional_set.lua
@@ -1,1 +1,11 @@
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 10 -- Number of line before the page go down or up
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
