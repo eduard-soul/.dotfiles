cc16903c0b22c7691c1a33a0c855a88e99db9c9e
Eduard <ylane9@live.fr>
2022-10-22 10:23:45 +0200

    change of keyboard allias

--- alias.zsh
+++ alias.zsh
@@ -3,3 +3,5 @@ alias pbpaste='xsel --clipboard --output'
 alias vi="nvim"
 alias cat='bat --style=plain'
 alias ygg="sudo sh ~/Desktop/Other/script_resolv.sh"
+alias accent="setxkbmap -layout us -variant intl"
+alias pasaccent="setxkbmap -layout us"
