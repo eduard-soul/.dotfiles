1c4f2b7b203ba82046871aa80a5fee7781a99dbe
Eduard <ylane9@live.fr>
2022-10-22 10:22:43 +0200

    smart indent

--- additional_set.lua
+++ additional_set.lua
@@ -5,7 +5,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.smartindent = true
+-- vim.opt.smartindent = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
