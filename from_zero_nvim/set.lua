dc686a7870c2b51d4702c8027ecba272df4c33e5
Eduard Tataru MacOS <eduard.tataru@mailo.com>
2022-11-14 14:44:51 +0100

    some line of nvim config added

---
+++ from_zero_nvim/set.lua
@@ -0,0 +1,5 @@
+vim.opt.guicursor = ""		-- change the gui cursor to the box one of visual
+
+vim.opt.nu = true 		-- display line numbers
+vim.opt.relativenumber = true 	-- display line numbers relative to cursor
+
