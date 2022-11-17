cb8500b74c8bf0028cb95bf8bdd56b4108561e57
Eduard Tataru MacOS <eduard.tataru@mailo.com>
2022-11-17 19:40:37 +0100

    removing hightlight on search

--- from_zero_nvim/set.lua
+++ from_zero_nvim/set.lua
@@ -1,15 +1,18 @@
-vim.opt.guicursor = ""		-- change the gui cursor to the box one of visual
+vim.opt.guicursor = ""		    -- change the gui cursor to the box one of visual
 
-vim.opt.nu = true 		-- display line numbers
+vim.opt.nu = true 		        -- display line numbers
 vim.opt.relativenumber = true 	-- display line numbers relative to cursor
 
-vim.opt.errorbells = false	-- remove errors bells (to clarify) 
+vim.opt.errorbells = false	    -- remove errors bells (to clarify) 
 
-vim.opt.tabstop = 4		-- size of tabs
-vim.opt.softtabstop = 4		-- size of tabs while editing
-vim.opt.shiftwidth = 4		-- number of space of (auto)indent
-vim.opt.expandtab = true	-- on insert mode: Use the appropriate number of spaces
+vim.opt.tabstop = 4		        -- size of tabs
+vim.opt.softtabstop = 4		    -- size of tabs while editing
+vim.opt.shiftwidth = 4		    -- number of space of (auto)indent
+vim.opt.expandtab = true	    -- on insert mode: Use the appropriate number of spaces
 
-vim.opt.smartindent = true	-- do smart autoindenting when starting a new line. 
+vim.opt.smartindent = true	    -- do smart autoindenting when starting a new line. 
 
-vim.opt.wrap = true -- set the wrapping or not of the line when the line goes outside of the screen
+vim.opt.wrap = true             -- set the wrapping or not of the line when the line goes outside of the screen
+
+vim.opt.hlsearch = false        -- set highlighting of search 
+
