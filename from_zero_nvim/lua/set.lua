73e8108068d17086957eca9261012f23601a0521
Eduard Tataru MacOS <eduard.tataru@mailo.com>
2022-12-11 13:59:25 +0100

    signcolumn removed

--- from_zero_nvim/lua/set.lua
+++ from_zero_nvim/lua/set.lua
@@ -22,3 +22,5 @@ vim.opt.colorcolumn = "80"      -- make a column at 80 chars
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
 vim.opt.updatetime = 50
+
+vim.opt.signcolumn = 'no'       -- removing signcolumn, the one on the left of the line numbers one
